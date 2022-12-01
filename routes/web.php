0f6e98555327d57acb28d066a02b591387de9976
4ntim4tter <4ntim4tter@gmail.com>
2022-12-01 18:56:31 +0100

    welcome page

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-function getContacts () {
+function getContacts()
+{
     $cont_data = [
-        1 => ['name' => 'Name 1', 'phone' => '1234567890'],
-        2 => ['name' => 'Name 2', 'phone' => '2345678901'],
-        3 => ['name' => 'Name 3', 'phone' => '3456789012'],
+        1 => ['id' => 1, 'name' => 'Name 1', 'phone' => '1234567890'],
+        2 => ['id' => 2, 'name' => 'Name 2', 'phone' => '2345678901'],
+        3 => ['id' => 3, 'name' => 'Name 3', 'phone' => '3456789012'],
     ];
 
     return $cont_data;
