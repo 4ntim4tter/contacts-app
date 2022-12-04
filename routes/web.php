451900c5b59ee0fd6c9ca8eb5c7f1471e6f1c779
4ntim4tter <4ntim4tter@gmail.com>
2022-12-04 20:32:47 +0100

    xamp mysql

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ContactCreateController;
+use App\Http\Controllers\ContactNoteController;
 use App\Http\Controllers\ContactShowController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,29 +22,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-function getContacts()
-{
-    $cont_data = [
-        1 => ['id' => 1, 'name' => 'Name 1', 'phone' => '1234567890'],
-        2 => ['id' => 2, 'name' => 'Name 2', 'phone' => '2345678901'],
-        3 => ['id' => 3, 'name' => 'Name 3', 'phone' => '3456789012'],
-    ];
-
-    return $cont_data;
-}
-
 Route::get('/', WelcomeController::class);
 
-Route::controller(ContactController::class)->name('contacts.')->group(function(){
-    
-    Route::get('/contacts', 'index')->name('index');
-    
-    Route::get('/contacts/create', 'create')->name('create');
-    
-    Route::get('/contacts/{id}', 'show')->name('show');
+Route::controller(ContactController::class)->name('contacts.')->group(function () {
 
+    Route::get('/contacts', 'index')->name('index');
+
+    Route::get('/contacts/create', 'create')->name('create');
+
+    Route::get('/contacts/{id}', 'show')->name('show');
 });
 
+Route::resource('/companies', CompanyController::class);
+
+Route::resources(['/tags' => TagController::class, '/tasks' => TaskController::class]);
+
+// Route::resource('/activities', ActivityController::class)->except(['index', 'show'])->names([
+//     'index' => 'activities.all',
+//     'show' => 'activities.view'
+// ]);
+
+Route::resource('/activities', ActivityController::class)->except(['index', 'show'])->parameters([
+    'activities' => 'active'
+]);
+
+Route::resource('/contacts.notes', ContactNoteController::class)->shallow(true);
 
 Route::fallback(function () {
     return "<h1>Sorry this route doesn't exist.</h1>";
