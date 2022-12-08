da4cd0ed8f63ab585ce4eae80313ff495aab1b9f
Anes <4ntim4tter@gmail.com>
2022-12-08 09:14:56 +0100

    notes model

---
+++ app/Models/company.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class company extends Model
+{
+    use HasFactory;
+
+    // protected $table = "app_companies";
+    // protected $primaryKey = "_id";
+}
